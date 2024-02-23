f7d5def33790e9bbd7f445b30ef57b702d2fd9b1
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-02-23 08:56:54 +0700

    tes qr code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\product;
 
 class ProductController extends Controller
 {
@@ -10,5 +11,27 @@ class ProductController extends Controller
 
         $products = product::all();
         return view('index',compact('products'));
+    }
+
+    public function create(){
+
+        return view('create');
+    }
+
+    public function store(Request $request){
+        $number = mt_rand(1000000000, 9999999999);
+        
+        if ($this->productCodeExists($number)){
+            $number = mt_rand(1000000000, 9999999999);
+        }
+
+        $request['product_code'] = $number;
+        Product::create($request->all());
+
+        return redirect('/');
+    }
+
+    public function productCodeExists($number){
+        return product::whereProductCode($number)->exists();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/',[ProductController::class,'index']);
+Route::get('/create',[ProductController::class,'create']);
+Route::post('/post',[ProductController::Class,'store']);
