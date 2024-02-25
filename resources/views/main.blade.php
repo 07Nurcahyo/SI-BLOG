17b374265b645bfe3145ec76c1ef54c3cbaa8567
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-02-25 09:27:14 +0700

    progress main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SI-BLOG</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
-    <link rel="stylesheet" href="resources/css/style.css"></link>
+    <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body >
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">SI-BLOG🗿</a>
+    <img src="img/jti.png" class="logo-jti" alt="">
+    <a class="navbar-brand" href="#">SI-BLOG</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -38,7 +39,7 @@
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-success" type="submit">Search</button>
       </form>
     </div>
   </div>
