e7f912cb10948aab5c8c3c0357f56e1fcbcc2e52
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-02-27 22:30:29 +0700

    update progress main

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,6 +10,7 @@
 </head>
 <body >
 
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <img src="img/jti.png" class="logo-jti" alt="">
@@ -19,27 +20,13 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-success" type="submit">Search</button>
+      <form class="d-flex">
+        <button type="button" class="btn btn-success rounded-3">Login admin
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person logoadmin" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+          </svg>
+        </button>
       </form>
     </div>
   </div>
