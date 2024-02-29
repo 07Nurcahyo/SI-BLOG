07eaf57a1e7cbdf7e3bb126ac68385f6b7863dbf
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-02-29 16:04:08 +0700

    update form login admin

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -33,19 +33,29 @@
 </nav>
 
 <!-- form login admin -->
-<div class="modal fade" id="exampleModal" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal modal-centered fade" id="exampleModal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-sm">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Login Admin</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Username</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Passowrd...">
+          </div>
+        </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Login</button>
       </div>
     </div>
   </div>
