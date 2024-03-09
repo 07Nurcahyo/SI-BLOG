fe99f081bfb0fc8ca682aded5c2c7b5ab7ccf17c
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-03-09 17:53:36 +0700

    update front end

---
+++ public/js/style.js
@@ -0,0 +1,3 @@
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+initMDB({ Ripple });
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,74 +10,82 @@
 </head>
 <body >
 
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <img src="img/jti.png" class="logo-jti" alt="">
-    <a class="navbar-brand" href="#">SI-BLOG</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      </ul>
-      <form class="d-flex">
-        <button type="button" class="btn btn-success rounded-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Login admin🙍‍♂️</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
-<!-- form login admin -->
-<div class="modal modal-centered fade" id="exampleModal" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-sm">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Login Admin</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        @if(session('error'))
-          <div class="alert alert-danger">
-              <b>Opps!</b> {{session('error')}}
-          </div>
-        @endif
-        <form action="{{ route('actionlogin') }}" method="post">
-        @csrf
-          <div class="mb-3">
-            <label class="form-label">Username</label>
-            <input type="username" name="username" class="form-control">
-            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" placeholder="Masukkan Passowrd...">
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-            <button type="submit" class="btn btn-primary">Login</button>
-          </div>
+<div class="">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <img src="img/jti.png" class="logo-jti" alt="">
+      <a class="navbar-brand" href="#">SI-BLOG</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        </ul>
+        <form class="d-flex">
+          <button type="button" class="btn btn-light rounded-4" data-bs-toggle="modal" data-bs-target="#exampleModal">Login admin🙍‍♂️</button>
         </form>
       </div>
     </div>
-  </div>
-</div>
+  </nav>
 
-<!-- Content -->
-<!-- <div class="text-center">
-  <h3>Selamat Datang</h3> -->
-  <section class="search" id="search">
-    <!-- <div class="content"> -->
-      <div class="input-group mb-3 searchfield">
-        <input type="text" class="form-control searchfield" placeholder="Cari Buku..." aria-label="" aria-describedby="basic-addon1">
-        <div class="input-group-prepend">
-          <button class="btn btn-secondary btncari" type="button">Cari</button>
+  <!-- form login admin -->
+  <div class="modal modal-centered fade" id="exampleModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Login Admin</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          @if(session('error'))
+            <div class="alert alert-danger">
+                <b>Opps!</b> {{session('error')}}
+            </div>
+          @endif
+          <form action="{{ route('actionlogin') }}" method="post">
+          @csrf
+            <div class="mb-3">
+              <label class="form-label">Username</label>
+              <input type="username" name="username" class="form-control">
+              <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Password</label>
+              <input type="password" name="password" class="form-control" placeholder="Masukkan Passowrd...">
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+              <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+          </form>
         </div>
       </div>
-    <!-- </div> -->
-  </section>
-<!-- </div> -->
+    </div>
+  </div>
+
+  <!-- Content -->
+    <section class="search" id="search">
+      <div class="content text-center">
+          <h5>Selamat Datang</h5>
+          <h2>Portal Layanan</h2>
+          <h2 class="jti">Ruang Baca JTI</h2>
+          <div class="input-group searchbar">
+            <input type="search" class="form-control" placeholder="Cari Buku.."/>
+            <button type="button" class="btn btn-primary btn-lg" data-mdb-ripple-init>Cari</button>
+          </div>
+      </div>
+    </section>
+    <button type="button" class="btn btn-light btn-lg tes py-2 px-4 start-50 translate-middle mt-1 rounded-5">Lihat Semua Buku</button>
+    <section class="koleksi" id="koleksi">
+      <div class="content text-center">
+        <p>Koleksi Ruang Baca</p>
+      </div>
+    </section>
+
+</div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/style.js"></script>
 </body>
 </html>
