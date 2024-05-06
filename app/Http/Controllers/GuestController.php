b767e727bafc4ccce8dbd5af4cd187d5921f66d8
mochamad rizaldy <mohammadrizaldy16@gmail.com>
2024-05-06 07:42:17 +0700

    .

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -11,6 +11,25 @@ class GuestController extends Controller
     }
 
     public function list(){
+        $books = [
+            [
+                'title' => 'Book 1',
+                'author' => 'Author 1',
+                'publisher' => 'Publisher 1',
+                'year' => '2020',
+                'category' => 'Category 1'
+            ],
+            [
+                'title' => 'Book 2',
+                'author' => 'Author 2',
+                'publisher' => 'Publisher 2',
+                'year' => '2019',
+                'category' => 'Category 2'
+            ],
+            // Add more books as needed
+        ];
+        
+        return view('books.index', ['books' => $books]);
         return view('guest.listbook');
     }
 
