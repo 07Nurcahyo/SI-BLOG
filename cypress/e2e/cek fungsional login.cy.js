379c6852c2251201db6a759b83d62b6cfe239897
Zhafirah350 <zhafirah.arinusi3005@gmail.com>
2024-06-01 13:05:00 +0700

    done bang

--- cypress/e2e/cek fungsional login.cy.js
+++ cypress/e2e/cek fungsional login.cy.js
@@ -9,39 +9,44 @@ describe('SI-BLOG tests', () => {
         cy.get('input[name="password"]').type('root')
         cy.get('form').submit()
         cy.get('h1').should('have.text', 'Dashboard')
-        cy.visit('/actionlogout')
+        cy.visit('logout')
     })
 
     it('Masukkan username tidak valid dan password valid', () => {
         cy.get('input[name="username"]').type('fafifu')
         cy.get('input[name="password"]').type('root')
         cy.get('form').submit()
-        cy.get('h1').should('have.text', 'Dashboard')
-        cy.visit('/actionlogout')
+        // cy.get('.alert alert-warning alert-dismissible fade show').should('have.text', 'Opps!')
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Pastikan kembali username dan password yang dimasukkan sudah benar')
+        })
     })
 
     it('Masukkan username valid dan password tidak valid', () => {
         cy.get('input[name="username"]').type('admin')
         cy.get('input[name="password"]').type('wasweswos')
         cy.get('form').submit()
-        cy.get('h1').should('have.text', 'Dashboard')
-        cy.visit('/actionlogout')
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Pastikan kembali username dan password yang dimasukkan sudah benar')
+        })
     })
 
     it('Masukkan username dan password tidak valid', () => {
         cy.get('input[name="username"]').type('fafifu')
         cy.get('input[name="password"]').type('wasweswos')
         cy.get('form').submit()
-        cy.get('h1').should('have.text', 'Dashboard')
-        cy.visit('/actionlogout')
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Pastikan kembali username dan password yang dimasukkan sudah benar')
+        })
     })
 
     it('username dan password kosong', () => {
-        cy.get('input[name="username"]').type('')
-        cy.get('input[name="password"]').type('')
+        cy.get('input[name="username"]')
+        cy.get('input[name="password"]')
         cy.get('form').submit()
-        cy.get('h1').should('have.text', 'Dashboard')
-        cy.visit('/actionlogout')
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Pastikan kembali username dan password yang dimasukkan sudah benar')
+        })
     })
   })
   
