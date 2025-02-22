9cbf97935907e8cd04f0be94b7e8cabf77d4f6b5
07Nurcahyo <bagusnurcahyo4@gmail.com>
2025-02-22 17:47:23 +0700

    update error

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            'laravel-datatables-vite': '/node_modules/laravel-datatables-vite',
+        },
+    },
 });
